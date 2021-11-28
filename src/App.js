c34fd9750a4f8d731cb557faf16fb17c9486dc98
Toni <01tonydimitrov@gmail.com>
2021-11-28 15:16:49 +0200

    Initial components

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+import './App.css';
+
+function App() {
+  return (
+    <header>
+      <Nav />
+      <Main>
+      </Main>
+      <Footer/>
+    </header>
+  );
+}
+
+export default App;
